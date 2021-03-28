2ff5b48a0196920688fe9dd1ded4b14536d462f0
Dammyton <owolabidamilola98@gmail.com>
2021-03-28 11:45:34 +0100

    api updated

--- src/components/APIComponents/fetchStarships.js
+++ src/components/APIComponents/fetchStarships.js
@@ -1,7 +1,7 @@
 import API from "../API";
 
 export const fetchStarships = async () => {
-  return await API.get(`starships`)
+  return await API.get(`starships.`)
     .then((results) => {
       return results.data;
     })
